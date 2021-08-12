97680d5a23d83560aad1b7d87fbaedcf829b1fa4
Felix-Silas <ygeuntae1208@naver.com>
2021-08-12 16:18:17 +0900

    first commit

---
+++ app/src/main/java/com/example/todolist2/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.todolist2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.nio.charset.MalformedInputException;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
